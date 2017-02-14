aab89dcdb02688b65e00753e1491bd97078813ef
pablo-pacheco <pablo.pacheco@origgami.com.br>
2017-02-14 15:45:21 -0200

    parametrize selecize options

--- classes/class-alg-wc-cp-selectize.php
+++ classes/class-alg-wc-cp-selectize.php
@@ -24,7 +24,7 @@ if ( ! class_exists( 'Alg_WC_CP_Selectize' ) ) {
 		 * @since   1.0.0
 		 */
 		public static function enqueue_scripts(){
-			wp_register_script( 'selectize', 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js', array( 'jquery' ), false, true );
+			wp_register_script( 'selectize', 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js', array( 'jquery','jquery-ui-sortable' ), false, true );
 			wp_enqueue_script( 'selectize' );
 			wp_register_style( 'selectize', 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.default.min.css', array(), false );
 			wp_enqueue_style( 'selectize' );
@@ -35,19 +35,25 @@ if ( ! class_exists( 'Alg_WC_CP_Selectize' ) ) {
 		 *
 		 * @version 1.0.0
 		 * @since   1.0.0
+		 * @param array $args
 		 */
-		public static function load_selectize(){
+		public static function load_selectize( $args = array() ) {
+			$opts = wp_parse_args( $args, array(
+				'plugins' => array( 'drag_drop', 'remove_button' ),
+				'persist' => false,
+			) );
+
 			$js = "
-					jQuery(document).ready(function($){
-						var selectize_inputs = $('.selectize_drag_drop');
-						selectize_inputs.each(function(){						
-							var select = jQuery(this).selectize({
-								plugins: ['drag_drop','remove_button'],
-								persist: false,
-							});								
-						});
-					})
-				";
+				var opts = '" . wp_json_encode( $opts ) . "';
+				opts = JSON.parse(opts);
+				console.log(opts);
+				jQuery(document).ready(function($){
+					var selectize_inputs = $('.selectize_drag_drop');
+					selectize_inputs.each(function(){						
+						var select = jQuery(this).selectize(opts);								
+					});
+				})
+			";
 			wp_add_inline_script( 'selectize', $js );
 		}
 	}
